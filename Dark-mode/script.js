3097d845ee191cdec63422c8df14dc384c3c479c
Mylan Brasier <mylanbrasierpro@gmail.com>
2025-12-09 13:33:24 +0100

    Dark-mode

--- Dark-mode/script.js
+++ Dark-mode/script.js
@@ -3,5 +3,6 @@ const dark = document.querySelector('.dark') ;
 const btn = document.querySelector('.btn') ; 
     
 btn.addEventListener('click',() => {
-    dark.style.backgrounColor = "black";
+   const element = document.body;
+   element.classList.toggle("dark");
 })
